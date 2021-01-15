eb48073a9133d62d991a784cc7b914c9958c94dc
laion12 <pandaerox@gmail.com>
2021-01-14 23:22:44 -0600

    loader upgrade

--- src/app/assets/js/functions.js
+++ src/app/assets/js/functions.js
@@ -1,6 +1,19 @@
 M.AutoInit();
-
+window.addEventListener('load',()=>{
+  setTimeout(load,2000);
+  function load(){
+    document.getElementById('loading').className='animate__animated animate__fadeOut animate__delay-0.5s display:none hide';
+    document.getElementById('loading1').className='animate__animated animate__fadeOut animate__delay-0.5s display:none hide';
+    document.getElementById('loading2').className='animate__animated animate__fadeOut animate__delay-0.5s display:none hide';
+    document.getElementById('contend').className='animate__animated animate__fadeIn animate__delay-0.5s';
+    setTimeout(change,1000);
+    function change(){
+      document.getElementById('contend').className=' ';
+    }
+  }
+})
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
-    var instances = M.Sidenav.init(elems, options);
+    var instances = M.Sidenav.init(elems);
   });
+
