4cae6dc1a09ef62ea102d1cf837d29c0744b8be0
laion12 <pandaerox@gmail.com>
2021-01-20 02:14:34 -0600

    update responsive design

--- src/app/assets/js/functions.js
+++ src/app/assets/js/functions.js
@@ -6,20 +6,22 @@ window.addEventListener('load',()=>{
     document.getElementById('loading1').className='animate__animated animate__fadeOut animate__delay-0.5s display:none hide';
     document.getElementById('loading2').className='animate__animated animate__fadeOut animate__delay-0.5s display:none hide';
     document.getElementById('contend').className='animate__animated animate__fadeIn animate__delay-0.5s';
+    document.getElementById('main').className='animate__animated animate__fadeIn animate__delay-0.5s';
+    document.getElementById('footer').className='animate__animated animate__fadeIn animate__delay-0.5s';
     setTimeout(change,1000);
     function change(){
       document.getElementById('contend').className=' ';
+      document.getElementById('main').className=' ';
+      document.getElementById('footer').className=' ';
     }
   }
 })
-if(document.addEventListener("wheel")){
-  document.addEventListener("wheel", function(){
-    document.getElementById('nav').className='navbar-fixed';
-  })
-}
+document.addEventListener("wheel", function(){
+  document.getElementById('nav').className='navbar-fixed';
+})
 
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.sidenav');
     var instances = M.Sidenav.init(elems);
-  });
+});
 
