f2341c52b0d5dcc0204ff3d87dfd1c866a74ace6
laion12 <pandaerox@gmail.com>
2021-02-26 04:17:48 -0600

    update form data

--- src/app/assets/js/fuctions-anime-create.js
+++ src/app/assets/js/fuctions-anime-create.js
@@ -1,3 +1,7 @@
 document.addEventListener("wheel", function(){
     document.getElementById('nav1').className='navbar-fixed';
 })
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.datepicker');
+    var instances = M.Datepicker.init(elems);
+  });
