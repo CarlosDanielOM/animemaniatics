1ac0a1db4f1180b2a87553e9f57d280415243803
laion12 <pandaerox@gmail.com>
2021-02-28 03:54:05 -0600

    update form2

--- src/app/assets/js/fuctions-anime-create.js
+++ src/app/assets/js/fuctions-anime-create.js
@@ -5,3 +5,27 @@ document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.datepicker');
     var instances = M.Datepicker.init(elems);
   });
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('select');
+  var instances = M.FormSelect.init(elems);
+});
+
+// var emision=document.getElementById('emision');
+// var finish=document.getElementById('finish');
+// var checked=document.getElementById('check');
+// var dt1=document.getElementById('date1');
+// var dt2=document.getElementById('date2');
+// emision.addEventListener("click", function(){
+//     checked.classList.remove("content-date-check");
+//     checked.classList.remove("content-date-check2");
+//     checked.classList.add("content-date-check1");
+//     dt1.classList.remove("disable");
+//     dt2.classList.add("disable");
+// })
+// finish.addEventListener("click", function(){
+//     checked.classList.remove("content-date-check");
+//     checked.classList.remove("content-date-check1");
+//     checked.classList.add("content-date-check2");
+//     dt1.classList.remove("disable");
+//     dt1.classList.remove("disable");
+// })
