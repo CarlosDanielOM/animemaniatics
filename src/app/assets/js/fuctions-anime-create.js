0efb47c0efa462a66a4bae23b4d1eaee811ec243
laion12 <pandaerox@gmail.com>
2021-03-01 06:28:18 -0600

    update form and card

--- src/app/assets/js/fuctions-anime-create.js
+++ src/app/assets/js/fuctions-anime-create.js
@@ -10,22 +10,15 @@ document.addEventListener('DOMContentLoaded', function() {
   var instances = M.FormSelect.init(elems);
 });
 
-// var emision=document.getElementById('emision');
-// var finish=document.getElementById('finish');
-// var checked=document.getElementById('check');
-// var dt1=document.getElementById('date1');
-// var dt2=document.getElementById('date2');
-// emision.addEventListener("click", function(){
-//     checked.classList.remove("content-date-check");
-//     checked.classList.remove("content-date-check2");
-//     checked.classList.add("content-date-check1");
-//     dt1.classList.remove("disable");
-//     dt2.classList.add("disable");
-// })
-// finish.addEventListener("click", function(){
-//     checked.classList.remove("content-date-check");
-//     checked.classList.remove("content-date-check1");
-//     checked.classList.add("content-date-check2");
-//     dt1.classList.remove("disable");
-//     dt1.classList.remove("disable");
-// })
+var emision=document.getElementById('emision');
+var finish=document.getElementById('finish');
+var dt1=document.getElementById('dt1');
+var dt2=document.getElementById('dt2');
+emision.addEventListener("click", function(){
+  dt1.classList.remove("disable");
+  dt2.classList.add("disable");
+})
+finish.addEventListener("click", function(){
+  dt1.classList.remove("disable");
+  dt2.classList.remove("disable");
+})
