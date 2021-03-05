9f7f24640512b4f648721d5ab5a12dbd0ed597ff
laion12 <pandaerox@gmail.com>
2021-03-04 23:38:40 -0600

    optimization 4/3/2021

--- src/app/assets/js/fuctions-anime-create.js
+++ src/app/assets/js/fuctions-anime-create.js
@@ -24,11 +24,11 @@ var dt2=document.getElementById('dt2');
 emision.addEventListener("click", function(){
   dt1.classList.remove("disable");
   dt2.classList.add("disable");
-})
+});
 finish.addEventListener("click", function(){
   dt1.classList.remove("disable");
   dt2.classList.remove("disable");
-})
+});
 
 var btn_info=document.getElementById('info');
 var card=document.getElementById('the-card');
@@ -37,8 +37,8 @@ var btn_exit=document.getElementById('exit');
 btn_info.addEventListener("click",function(){
   cnt_info.classList.remove("content-info");
   cnt_info.classList.add("content-info2");
-})
+});
 btn_exit.addEventListener("click",function(){
   cnt_info.classList.remove("content-info2");
   cnt_info.classList.add("content-info");
-})
+});
